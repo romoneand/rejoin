ef1bd3c6007526438f87672017c7763513e296ba
romoneand <104769745+romoneand@users.noreply.github.com>
2022-05-04 00:41:02 +0700

    Update Rejoin.lua

--- Rejoin.lua
+++ Rejoin.lua
@@ -1,8 +1,24 @@
-Print("rejoin")  
-   while true do wait()
-       getgenv().rejoin = game.GetSerice("Corngui").RobloxPromptGui.promptOverlaymChildAdded:Connect(function(Kick)
-           if Kick.Name =='ErrorPrompt' and Kick:FindFirstChild('MessageArea') and Kick:MessageArea:FindFirstChild("ErrorFrame") then   
-               game:GetService("TeleportService"):Teleport(game.PlaceId)    
-           end   
-        end) 
-      end
+print("Rejoin On Kick Script Loaded Successfully")
+
+while true do wait()
+
+getgenv().rejoin = game:GetService("CoreGui").RobloxPromptGui.promptOverlay.ChildAdded:Connect(function(Kick)
+
+    if Kick.Name == 'ErrorPrompt' and Kick:FindFirstChild('MessageArea') and Kick.MessageArea:FindFirstChild("ErrorFrame") then
+
+        game:GetService("TeleportService"):Teleport(game.PlaceId)
+
+    end
+
+end)
+
+end
+
+
+
+
+
+
+
+
+
